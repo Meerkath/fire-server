279219755bdd34e71c58188a71b3d6aa811c2f18
Alexandre Viale <alexandreviale.06gmail.com>
2021-12-18 16:37:02 +0100

    token + refreshToken + users + friends

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  AEPort: 3000, // port of this app
+  mongoDBLink:"mongodb+srv://meerkath:GmItTxpUoc8fXHsJ@cluster0.pcf53.mongodb.net/meerssenger?retryWrites=true&w=majority" // Link of your mongoDB Database
+}
+
+module.exports = config;
